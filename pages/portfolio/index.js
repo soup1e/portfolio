11e2622002b45d8716e47e211737a313d491cb31
Sam Adams <sam.adams7785@gmail.com>
2023-06-21 16:15:59 -0700

    added granato auto web

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -7,6 +7,73 @@ export default function Portfolio() {
   return (
     <Layout title="portfolio">
       <section
+        id="granato auto"
+        className="flex flex-col justify-center text-gray-50 min-h-screen max-w-6xl mx-auto p-4 sm:px-6 h-full "
+      >
+        <section
+          id="image"
+          className="max-w-sm mx-auto md:max-w-none grid md:grid-cols-2 gap-6 md:gap-8 lg:gap-12 xl:gap-16 items-center order-last"
+        >
+          <Link href="https://granatoautodetailing.com" target="_blank">
+            <Image
+              className="w-full h-full object-cover transition "
+              src="/granatoauto.PNG"
+              width="540"
+              height="303"
+              alt="granatoauto!"
+            />
+          </Link>
+          <section id="details">
+            <div className="mb-3">
+              <ul className="flex flex-wrap text-xs font-medium -m-1">
+                <li className="m-1 inline-flex text-center text-gray-100 py-1 px-3 rounded-full bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out">
+                  JavaScript
+                </li>
+                <li className="m-1 inline-flex text-center text-gray-100 py-1 px-3 rounded-full bg-blue-600 hover:bg-blue-700 transition duration-150 ease-in-out">
+                  TailwindCSS
+                </li>
+                <li className="m-1 inline-flex text-center text-gray-100 py-1 px-3 rounded-full bg-blue-600 hover:bg-blue-700 transition duration-150 ease-in-out">
+                  NextJS
+                </li>
+                <li className="m-1 inline-flex text-center text-gray-100 py-1 px-3 rounded-full bg-blue-600 hover:bg-blue-700 transition duration-150 ease-in-out">
+                  EmailJS
+                </li>
+                <li className="m-1 inline-flex text-center text-gray-100 py-1 px-3 rounded-full bg-indigo-600 hover:bg-indigo-700 transition duration-150 ease-in-out">
+                  Netlify
+                </li>
+              </ul>
+            </div>
+
+            <h1 className="text-2xl lg:text-3xl font-bold leading-tight mb-2">
+              <Link
+                className="hover:text-gray-500 transition duration-150 ease-in-out"
+                href="https://github.com/soup1e/granato-auto-web"
+                target="_blank"
+              >
+                Granato Auto Detailing
+              </Link>
+            </h1>
+            <main className="text-lg text-gray-400 flex-grow">
+              Granato Auto Detailing is a Next.js page made for a client that
+              needed a website fast for their growing business. It features a
+              Contact page using EmailJS to get directly in touch with Micheal.
+            </main>
+
+            <footer className="flex items-center mt-4 gap-4">
+              <Link
+                className="ring ring-violet-500 hover:bg-indigo-900 text-gray-100 py-1 px-3 rounded-full lg:text-md text-xs hover:transition-all duration-700"
+                href="https://github.com/soup1e/granato-auto-web"
+                target="_blank"
+              >
+                View Code
+              </Link>
+              <p className="text-gray-500">March 23, 2023</p>
+            </footer>
+          </section>
+        </section>
+      </section>
+
+      <section
         id="boathousebot"
         className="flex flex-col justify-center text-gray-50 min-h-screen max-w-6xl mx-auto p-4 sm:px-6 h-full"
       >
